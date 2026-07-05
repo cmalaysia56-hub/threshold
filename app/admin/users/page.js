609a1d319aaed9a8a61afe77dedcc85911d94db1
cmalaysia56-hub <cmalaysia56@gmail.com>
2026-07-05 17:08:34 -0500

    Create page.js

---
+++ app/admin/users/page.js
@@ -0,0 +1,8 @@
+export default function UserManagement() {
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold mb-4">User Management</h1>
+      <p>Manage application users here.</p>
+    </div>
+  );
+}
